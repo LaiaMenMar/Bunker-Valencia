b05a8bd2afa45fc863528580af55321f4e0cd220
LaiaMenMar <lamenmar@gmail.com>
2024-04-01 00:37:02 +0200

    Añadiendo los parametros de Boulder a ViewModel

--- app/src/main/java/com/laiamenmar/bunkervalencia/model/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.laiamenmar.bunkervalencia.model
-
-data class Note(
-    var id: String? = null,
-    var userId: String = "",
-    val title: String = "",
-    val content: String = ""
-)
