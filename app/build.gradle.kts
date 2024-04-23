75c79a758252073ef6088782c23421df158e9f41
LaiaMenMar <lamenmar@gmail.com>
2024-04-23 13:11:28 +0200

    Camera funcionando desde el Main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     //livedata
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
     //iconos
-    //implementation("androidx.compose.material:material-icons-extended-android:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended-android:1.5.0")
 
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
@@ -94,9 +94,29 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.5.0-alpha01")
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    // CameraX core library using the camera2 implementation
+    val camerax_version = "1.4.0-alpha05"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    //implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    // If you want to additionally use the CameraX VideoCapture library
+  //  implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    // If you want to additionally add CameraX ML Kit Vision Integration
+  //  implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    // If you want to additionally use the CameraX Extensions library
+   // implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+
     //inyeccion de dependencias
     // implementation("com.google.dagger:hilt-android:2.41")
 //    kapt("com.google.dagger:hilt-android-compiler:2.41")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
 
 
--- app/src/main/java/com/laiamenmar/bunkervalencia/ui/screens/home/Components.kt
+++ app/src/main/java/com/laiamenmar/bunkervalencia/ui/screens/home/Components.kt
@@ -1,9 +1,13 @@
 package com.laiamenmar.bunkervalencia.ui.screens.home
 
-import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,25 +15,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.CheckCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
@@ -38,6 +30,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -45,11 +38,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.laiamenmar.bunkervalencia.model.Constants_Climb
@@ -244,6 +236,7 @@ fun TitleScreen(title: String, modifier: Modifier) {
         modifier = modifier.padding(16.dp)
     )
 }
+
 
 
 
